bbcd19527ca89ad2b138dafaa7034df7ab28dd1e
Noel Rivas <noel@matrushka.com.mx>
2023-02-09 16:53:56 -0600

    Fixed the logo strip generator in pb_devel_generate

--- modules/pb_devel_generate/src/DemoGenerators/LogoStripGenerator.php
+++ modules/pb_devel_generate/src/DemoGenerators/LogoStripGenerator.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace Drupal\m3_page_builder\DemoGenerators;
+namespace Drupal\pb_devel_generate\DemoGenerators;
 
-use Drupal\m3_page_builder\DemoGeneratorInterface;
-use Drupal\m3_page_builder\GeneratorHelpers\LayoutNodeHelper;
-use Drupal\m3_page_builder\GeneratorHelpers\MediaHelper;
-use Drupal\m3_page_builder\GeneratorHelpers\ParagraphsHelper;
+use Drupal\pb_devel_generate\DemoGeneratorInterface;
+use Drupal\pb_devel_generate\GeneratorHelpers\LayoutNodeHelper;
+use Drupal\pb_devel_generate\GeneratorHelpers\MediaHelper;
+use Drupal\pb_devel_generate\GeneratorHelpers\ParagraphsHelper;
 use Drupal\Component\Utility\Random;
 
-class LogoCarouselGenerator implements DemoGeneratorInterface {
+class LogoStripGenerator implements DemoGeneratorInterface {
 
   /**
    * {@inheritdoc}
@@ -17,20 +17,15 @@ class LogoCarouselGenerator implements DemoGeneratorInterface {
     $messenger = \Drupal::messenger();
     $random = new Random();
 
-    $target_field = $values['pages_target_field'];
-    $page = LayoutNodeHelper::createNodeWithLayoutVariation('page', $target_field, 'Logo Carousels - ', $variation);
-
-    $page->set('field_content_brief', $random->sentences(4));
-    $preview_image_media = MediaHelper::createMediaImage();
-    $page->field_preview_image->target_id = $preview_image_media->id();
-    $page->save();
+    $target_field = $values['target_field'];
+    $page = LayoutNodeHelper::createNodeWithLayoutVariation('page', $target_field, 'Logo Strips - ', $variation);
 
     // Populate each of the regions of each of the layout paragraphs with styles demo content
     $content_paragraphs = [];
     $population_distribution = [
-      'prefix' => 'heading',
-      'column_a' => 'logo-carousel',
-      'suffix' => 'text-styles-demo',
+      'header' => 'heading',
+      'column_a' => 'logo-strip',
+      'footer' => 'rich-text',
     ];
 
     $layout_paragraphs = LayoutNodeHelper::getLayoutParagraphs($page, $target_field);
@@ -44,39 +39,34 @@ class LogoCarouselGenerator implements DemoGeneratorInterface {
 
       foreach($paragraph_regions as $region_name => $region_label) {
 
-        // We really don't want to generate a zillion carousels in tiny columns here.
-        if($paragraph_layout_name !== 'm3_one_column') {
+        // We really don't want to generate a zillion strips in tiny columns here.
+        if($paragraph_layout_name !== 'pb_one_column') {
           continue;
         }
 
         $populate_with = $population_distribution[$region_name];
         switch($populate_with) {
           case 'heading':
-            $paragraph_code = "$paragraph_layout_name--{$paragraph_behavior_settings['layout_paragraphs']['config']['additional']['classes']['custom_style']}";
+            $paragraph_code = "$paragraph_layout_name--{$paragraph_behavior_settings['layout_paragraphs']['config']['additional']['classes']['color_scheme']}";
             $content_paragraphs[] = ParagraphsHelper::createHeadingParagraph($region_name, $paragraph_uuid, $paragraph_code, $paragraph_code);
             break;
-          case 'logo-carousel':
-            // Create a fully random carousel
-            $content_paragraphs[] = ParagraphsHelper::createLogoCarouselParagraph($region_name, $paragraph_uuid);
+          case 'logo-strip':
+            // Create a fully random strip
+            $content_paragraphs[] = ParagraphsHelper::createLogoStripParagraph($region_name, $paragraph_uuid);
 
-            // Create a carousel populated with 4:3 aspect ratio images
+            // Create a strip populated with 4:3 aspect ratio images
             $num_images = mt_rand(12, 20);
             $images_4_3 = [];
             $media_image = MediaHelper::createMediaImageFromUrl("https://dummyimage.com/300x225/ED003A/fff.png", "300x225.png", '300x225') ;
             for($i=0; $i<$num_images; $i++) {
               $images_4_3[] = ParagraphsHelper::createImageParagraphFromMediaEntity($region_name, $paragraph_uuid, $media_image);
             }
-            // Max 3 columns
-            $content_paragraphs[] = ParagraphsHelper::createLogoCarouselParagraph($region_name, $paragraph_uuid, 3, $images_4_3);
 
-            // Max 4 columns
-            $content_paragraphs[] = ParagraphsHelper::createLogoCarouselParagraph($region_name, $paragraph_uuid, 4, $images_4_3);
+            // Grid style
+            $content_paragraphs[] = ParagraphsHelper::createLogoStripParagraph($region_name, $paragraph_uuid, 'grid', $images_4_3);
 
-            // Max 5 columns
-            $content_paragraphs[] = ParagraphsHelper::createLogoCarouselParagraph($region_name, $paragraph_uuid, 5, $images_4_3);
-
-            // Max 6 columns
-            $content_paragraphs[] = ParagraphsHelper::createLogoCarouselParagraph($region_name, $paragraph_uuid, 6, $images_4_3);
+            // Strip style
+            $content_paragraphs[] = ParagraphsHelper::createLogoStripParagraph($region_name, $paragraph_uuid, 'strip', $images_4_3);
             break;
         }
       }
@@ -91,17 +81,10 @@ class LogoCarouselGenerator implements DemoGeneratorInterface {
     }
 
     foreach($content_paragraphs as $para) {
-      // HACK ALERT: hardcoded filtering of compatible paragraph types
-      // TODO: programmatically exclude the paragraph types that are not compatible with the field definition
-      // and move all this code that repeats in every generator to a base implementation
-      $para_is_heading = $para->type->target_id == 'content_heading';
-      $field_is_intro = $target_field == 'field_intro';
-      if(!($para_is_heading && $field_is_intro)) {
-        $field_content[] = [
-          'target_id' => $para->id(),
-          'target_revision_id' => $para->getRevisionId()
-        ];
-      }
+      $field_content[] = [
+        'target_id' => $para->id(),
+        'target_revision_id' => $para->getRevisionId()
+      ];
     }
 
     $page->set($target_field, $field_content);
