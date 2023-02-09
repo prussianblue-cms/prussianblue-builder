32342c3dede62f036d4771410068eab11c4e57e1
Noel Rivas <noel@matrushka.com.mx>
2023-02-09 16:25:02 -0600

    Fixed the video generator in the pb_devel_generate module.

--- modules/pb_devel_generate/src/DemoGenerators/VideoGenerator.php
+++ modules/pb_devel_generate/src/DemoGenerators/VideoGenerator.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace Drupal\m3_page_builder\DemoGenerators;
+namespace Drupal\pb_devel_generate\DemoGenerators;
 
-use Drupal\m3_page_builder\DemoGeneratorInterface;
-use Drupal\m3_page_builder\GeneratorHelpers\LayoutNodeHelper;
-use Drupal\m3_page_builder\GeneratorHelpers\MediaHelper;
-use Drupal\m3_page_builder\GeneratorHelpers\ParagraphsHelper;
+use Drupal\pb_devel_generate\DemoGeneratorInterface;
+use Drupal\pb_devel_generate\GeneratorHelpers\LayoutNodeHelper;
+use Drupal\pb_devel_generate\GeneratorHelpers\MediaHelper;
+use Drupal\pb_devel_generate\GeneratorHelpers\ParagraphsHelper;
 use Drupal\Component\Utility\Random;
 
-class VideosGenerator implements DemoGeneratorInterface {
+class VideoGenerator implements DemoGeneratorInterface {
   /**
    * {@inheritdoc}
    **/
@@ -16,27 +16,22 @@ class VideosGenerator implements DemoGeneratorInterface {
     $messenger = \Drupal::service('messenger');
     $random = new Random();
 
-    $target_field = $values['pages_target_field'];
-    $page = LayoutNodeHelper::createNodeWithLayoutVariation('page', $target_field, 'Videos - ', $variation);
-
-    $page->set('field_content_brief', $random->sentences(4));
-    $preview_image_media = MediaHelper::createMediaImage();
-    $page->field_preview_image->target_id = $preview_image_media->id();
-    $page->save();
+    $target_field = $values['target_field'];
+    $page = LayoutNodeHelper::createNodeWithLayoutVariation('pb_page', $target_field, 'Videos - ', $variation);
 
     // Populate each of the regions of each of the layout paragraphs with styles demo content
     $content_paragraphs = [];
     $population_distribution = [
-      'prefix' => 'heading',
+      'header' => 'heading',
       'column_a' => 'video',
       'column_b' => 'video',
       'column_c' => 'video',
       'column_d' => 'video',
-      'suffix' => null,
+      'footer' => null,
     ];
 
     $layout_paragraphs = LayoutNodeHelper::getLayoutParagraphs($page, $target_field);
-    $media_video = MediaHelper::createMediaVideoFromUrl('https://www.youtube.com/watch?v=1ygdAiDxKfI');
+    $media_video = MediaHelper::createMediaVideoFromUrl('https://www.youtube.com/watch?v=e4TFD2PfVPw');
 
     foreach($layout_paragraphs as $layout_paragraph_index => $layout_paragraph) {
       $paragraph_behavior_settings = $layout_paragraph->getAllBehaviorSettings();
@@ -49,7 +44,7 @@ class VideosGenerator implements DemoGeneratorInterface {
         $populate_with = $population_distribution[$region_name];
         switch($populate_with) {
           case 'heading':
-            $paragraph_code = "$paragraph_layout_name--{$paragraph_behavior_settings['layout_paragraphs']['config']['additional']['classes']['custom_style']}";
+            $paragraph_code = "$paragraph_layout_name--{$paragraph_behavior_settings['layout_paragraphs']['config']['additional']['classes']['color_scheme']}";
             $content_paragraphs[] = ParagraphsHelper::createHeadingParagraph($region_name, $paragraph_uuid, $paragraph_code, $paragraph_code);
             break;
           case 'video':
@@ -68,20 +63,13 @@ class VideosGenerator implements DemoGeneratorInterface {
     }
 
     foreach($content_paragraphs as $para) {
-      // HACK ALERT: hardcoded filtering of compatible paragraph types
-      // TODO: programmatically exclude the paragraph types that are not compatible with the field definition
-      // and move all this code that repeats in every generator to a base implementation
-      $para_is_heading = $para->type->target_id == 'content_heading';
-      $field_is_intro = $target_field == 'field_intro';
-      if(!($para_is_heading && $field_is_intro)) {
-        $field_content[] = [
-          'target_id' => $para->id(),
-          'target_revision_id' => $para->getRevisionId()
-        ];
-      }
+      $field_content[] = [
+        'target_id' => $para->id(),
+        'target_revision_id' => $para->getRevisionId()
+      ];
     }
 
-    $target_field = $values['pages_target_field'];
+    $target_field = $values['target_field'];
     $page->set($target_field, $field_content);
     $page->save();
 
